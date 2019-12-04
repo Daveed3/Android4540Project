92e386050ba9a83eea791ced0dcbd3a301544434
David <dsan2@calstatela.edu>
2019-12-04 13:17:19 -0800

    Lynne's Firebase

---
+++ app/src/main/java/com/example/gantt/Login.kt
@@ -0,0 +1,32 @@
+package com.example.gantt
+
+
+import android.os.Bundle
+import android.util.Log
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_login.*
+
+/**
+ * A simple [Fragment] subclass.
+ */
+class Login : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?): View? {
+
+        val email = email_edittext_register.text.toString()
+        val password = password_edittext_register.text.toString()
+
+        Log.d("Login", "Email is:" + email)
+        Log.d("Login", "Password: $password")
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+
+}
