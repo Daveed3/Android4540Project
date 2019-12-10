f42e565410c52abaa505ad2621b15e5673f6b18b
LYNNE <ltien@calstatela.edu>
2019-12-10 15:55:06 -0800

    Merge remote-tracking branch 'proj1/master'
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/gantt/MainActivity.kt
    #	build.gradle

--- app/src/main/java/com/example/gantt/MainActivity.kt
+++ app/src/main/java/com/example/gantt/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.gantt.databinding.ActivityMainBinding
 import androidx.databinding.DataBindingUtil
+import com.google.firebase.database.FirebaseDatabase
 
 class MainActivity : AppCompatActivity() {
 
@@ -12,5 +13,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.reference
+        println("this is the db:" + myRef)
+//        myRef.child("test").child("trainer").setValue("2")
+        myRef.child("trainer2").child("trainer1").setValue("Ash")
     }
 }
