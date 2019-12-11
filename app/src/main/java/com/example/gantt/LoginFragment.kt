1336799d3121a104e386e885d775aa8083c98366
LYNNE <ltien@calstatela.edu>
2019-12-11 00:39:13 -0800

    Merge branch 'master', remote-tracking branch 'remotes/proj1/master' into HEAD
    ERROR:     java.lang.IllegalArgumentException: Given String is empty or null
            at com.google.android.gms.common.internal.Preconditions.checkNotEmpty(Unknown Source:5)
            at com.google.firebase.auth.FirebaseAuth.createUserWithEmailAndPassword(com.google.firebase:firebase-auth@@19.2.0:255)
            at com.example.gantt.LoginFragment.onCreateView(LoginFragment.kt:47)
    
    # Conflicts:
    #	app/build.gradle
    #	app/src/main/java/com/example/gantt/MainActivity.kt
    #	build.gradle

--- app/src/main/java/com/example/gantt/LoginFragment.kt
+++ app/src/main/java/com/example/gantt/LoginFragment.kt
@@ -14,14 +14,23 @@ import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_login.*
 
 class LoginFragment : Fragment() {
-
+    public override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = FirebaseAuth.getInstance().currentUser
+//        updateUI(currentUser)
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val binding = DataBindingUtil.inflate<FragmentLoginBinding>(inflater,
             R.layout.fragment_login,container,false)
+
         //TODO: add functionality for login checking and loading database
+
+
+
         binding.loginButton.setOnClickListener { view : View ->
             view.findNavController().navigate(R.id.action_loginFragment_to_ganttFragment)
         }
@@ -34,15 +43,13 @@ class LoginFragment : Fragment() {
 
         println("nickname"+ binding.nameEdittextRegister.text.toString())
 
-        //Todo: firebase authentication to create user with email and password
+        //Todo: Firebase authentication to create user with email and password
         FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener {
             if (!it.isSuccessful) return@addOnCompleteListener
 
             //else if successful
-            Log.d("Login", "Login Sucess, Email is: + ${it.result?.user?.uid}")
-
+            Log.d("Login", "Login Sucess")
         }
-
         return binding.root
     }
 
