0e5b5f47b7b60482d5ee88ee8c1b5e069864fde8
David <dsan2@calstatela.edu>
2019-12-13 10:36:12 -0800

    Finshed off with fixing password box

--- app/src/main/java/com/example/gantt/ViewTaskFragment.kt
+++ app/src/main/java/com/example/gantt/ViewTaskFragment.kt
@@ -1,17 +1,25 @@
 package com.example.gantt
 
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
+import android.widget.LinearLayout
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
 import com.example.gantt.databinding.FragmentViewTaskBinding
 import com.google.firebase.database.DataSnapshot
+import kotlinx.android.synthetic.main.fragment_view_task.*
+import kotlinx.android.synthetic.main.fragment_view_task.view.*
+import kotlinx.android.synthetic.main.fragment_view_task.view.button_layout
 
 class ViewTaskFragment : Fragment() {
 
@@ -42,6 +50,24 @@ class ViewTaskFragment : Fragment() {
 ////        button = Button(context)
 //        Button<context> button = inflater.inflate(R.layout.fragment_view_task, null)
 //        binding.layout.addView()
+        var list = mutableListOf<Int>(1,2)
+
+//        var buttonLayout = view.findViewById<LinearLayout>(R.id.button_layout)
+
+//        Log.d("message", "Here is " + buttonLayout)
+//
+//        for (i in list) {
+//            var button = Button(context)
+//            button.layoutParams = ConstraintLayout.LayoutParams(ConstraintLayout.LayoutParams.WRAP_CONTENT, ConstraintLayout.LayoutParams.WRAP_CONTENT)
+//            button.id = 100 + i
+//            button.text = "Button" + i
+//            button.setOnClickListener(View.OnClickListener {
+//                button.text = "button " + i + " was clicked"
+//            })
+//            button.setBackgroundColor(Color.GREEN)
+//            buttonLayout.addView(button)
+//
+//        }
 
 
         return binding.root
